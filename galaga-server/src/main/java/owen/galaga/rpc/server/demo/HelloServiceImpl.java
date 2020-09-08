921661a833e8b05d9ec083499ac16052f4fe0103
wangyuan20 <wangyuan20@jd.com>
2020-09-08 18:08:46 +0800

    增加filter模块 优化spring schema扩展配置

--- galaga-server/src/main/java/owen/galaga/rpc/server/demo/HelloServiceImpl.java
+++ galaga-server/src/main/java/owen/galaga/rpc/server/demo/HelloServiceImpl.java
@@ -1,5 +1,7 @@
 package owen.galaga.rpc.server.demo;
 
+import org.springframework.stereotype.Service;
+
 import owen.galaga.rpc.demo.api.HelloService;
 
 /**
@@ -8,7 +10,7 @@ import owen.galaga.rpc.demo.api.HelloService;
  * @date 2020/6/10 18:40
  */
 
-
+@Service
 public class HelloServiceImpl implements HelloService {
 
     @Override
